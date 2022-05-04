dfd8d43e6e4a9fdc1048f3139ea33803d31e7ee1
kathleen101 <81065967+kathleen101@users.noreply.github.com>
2022-05-03 21:26:29 -0700

    Update users.py
    Used pylint to ensure pep 8 followed.

--- users.py
+++ users.py
@@ -5,6 +5,7 @@ All edits made by Kathleen Wong to incorporate logging issues.
 # pylint: disable=R0903
 import logging
 
+
 class Users():
     '''
     Contains user information
@@ -30,15 +31,15 @@ class UserCollection():
         Adds a new user to the collection
         '''
         if user_id in self.database:
-            # Rejects new status if status_id already exists
-            logging.error(f'Unable to modify {user_id} because it ' \
-                           'does not exist in UserCollection.')
-            logging.debug("UserCollection contains following users': " +
+            # Rejects new status if user_id already exists
+            logging.error('Unable to modify %s because it ' \
+                          'does not exist in UserCollection.', user_id)
+            logging.debug("UserCollection contains following users: %s",
                           ', '.join(self.database.keys()))
             return False
         new_user = Users(user_id, email, user_name, user_last_name)
         self.database[user_id] = new_user
-        logging.info(f'Successfully added user {user_id}!')
+        logging.info('Successfully added user %s!', user_id)
         return True
 
     def modify_user(self, user_id, email, user_name, user_last_name):
@@ -46,15 +47,15 @@ class UserCollection():
         Modifies an existing user
         '''
         if user_id not in self.database:
-            logging.error(f'Unable to modify {user_id} because it ' \
-                           'does not exist in UserCollection.')
-            logging.debug("UserCollection contains following status': " +
+            logging.error('Unable to modify %s because it ' \
+                          'does not exist in UserCollection.', user_id)
+            logging.debug("UserCollection contains following users': %s",
                           ', '.join(self.database.keys()))
             return False
         self.database[user_id].email = email
         self.database[user_id].user_name = user_name
         self.database[user_id].user_last_name = user_last_name
-        logging.info(f'Successfully modified status {user_id}!')
+        logging.info('Successfully modified user %s!', user_id)
         return True
 
     def delete_user(self, user_id):
@@ -63,13 +64,13 @@ class UserCollection():
         '''
         if user_id not in self.database:
             # Fails if status does not exist
-            logging.error(f'Unable to delete {user_id} because it ' \
-                          'does not exist in UserCollection.')
-            logging.debug("UserCollection contains following status': " +
+            logging.error('Unable to delete %s because it '
+                          'does not exist in UserCollection.', user_id)
+            logging.debug("UserCollection contains following user': %s,",
                           ', '.join(self.database.keys()))
             return False
         del self.database[user_id]
-        logging.info(f'Successfully deleted status {user_id}!')
+        logging.info('Successfully deleted user %s!', user_id)
         return True
 
     def search_user(self, user_id):
@@ -77,8 +78,8 @@ class UserCollection():
         Searches for user data
         '''
         if user_id not in self.database:
-            logging.error(f'Unable to find {user_id} in UserCollection.')
-            logging.debug("UserCollection contains following users': " +
+            logging.error('Unable to find %s in UserCollection.', user_id)
+            logging.debug("UserCollection contains following users': %s",
                           ', '.join(self.database.keys()))
             return Users(None, None, None, None)
         return self.database[user_id]
