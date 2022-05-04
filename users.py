9e373503fd1e6110abf27db8f71cf91251d152e2
kathleen101 <81065967+kathleen101@users.noreply.github.com>
2022-05-03 20:48:21 -0700

    Update users.py
    Committing with logging.

--- users.py
+++ users.py
@@ -1,8 +1,9 @@
 '''
 Classes for user information for the social network project
+All edits made by Kathleen Wong to incorporate logging issues.
 '''
 # pylint: disable=R0903
-
+import logging
 
 class Users():
     '''
@@ -30,9 +31,14 @@ class UserCollection():
         '''
         if user_id in self.database:
             # Rejects new status if status_id already exists
+            logging.error(f'Unable to modify {user_id} because it ' \
+                           'does not exist in UserCollection.')
+            logging.debug("UserCollection contains following users': " +
+                          ', '.join(self.database.keys()))
             return False
         new_user = Users(user_id, email, user_name, user_last_name)
         self.database[user_id] = new_user
+        logging.info(f'Successfully added user {user_id}!')
         return True
 
     def modify_user(self, user_id, email, user_name, user_last_name):
@@ -40,10 +46,15 @@ class UserCollection():
         Modifies an existing user
         '''
         if user_id not in self.database:
+            logging.error(f'Unable to modify {user_id} because it ' \
+                           'does not exist in UserCollection.')
+            logging.debug("UserCollection contains following status': " +
+                          ', '.join(self.database.keys()))
             return False
         self.database[user_id].email = email
         self.database[user_id].user_name = user_name
         self.database[user_id].user_last_name = user_last_name
+        logging.info(f'Successfully modified status {user_id}!')
         return True
 
     def delete_user(self, user_id):
@@ -51,8 +62,14 @@ class UserCollection():
         Deletes an existing user
         '''
         if user_id not in self.database:
+            # Fails if status does not exist
+            logging.error(f'Unable to delete {user_id} because it ' \
+                          'does not exist in UserCollection.')
+            logging.debug("UserCollection contains following status': " +
+                          ', '.join(self.database.keys()))
             return False
         del self.database[user_id]
+        logging.info(f'Successfully deleted status {user_id}!')
         return True
 
     def search_user(self, user_id):
@@ -60,5 +77,8 @@ class UserCollection():
         Searches for user data
         '''
         if user_id not in self.database:
+            logging.error(f'Unable to find {user_id} in UserCollection.')
+            logging.debug("UserCollection contains following users': " +
+                          ', '.join(self.database.keys()))
             return Users(None, None, None, None)
         return self.database[user_id]
